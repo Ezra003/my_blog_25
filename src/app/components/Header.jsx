8b34511669d0072ff6cff76a5078ca04453a77ab
Ezra003 <126328199+Ezra003@users.noreply.github.com>
2025-03-29 17:49:06 +0100

    Test darkmode

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,19 +2,19 @@
 import {
   Button,
   Navbar,
-  NavbarCollapse,
-  NavbarLink,
-  NavbarToggle,
   TextInput,
+  NavbarToggle,
+  NavbarLink,
+  NavbarCollapse,
 } from "flowbite-react";
 import Link from "next/link";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
+import { FaMoon, FaSun } from "react-icons/fa";
 import { usePathname } from "next/navigation";
-
+import { useTheme } from "next-themes";
 export default function Header() {
   const path = usePathname();
-
+  const { theme, setTheme } = useTheme();
   return (
     <Navbar className="border-b-2">
       <Link
@@ -38,14 +38,16 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-          <FaMoon />
+        <Button
+          className="w-12 h-10 hidden sm:inline"
+          color="gray"
+          pill
+          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        >
+          {theme === "light" ? <FaSun /> : <FaMoon />}
         </Button>
         <Link href="/sign-in">
-          <Button
-            className="bg-gradient-to-br from-purple-600 to-blue-500 text-white hover:bg-gradient-to-bl focus:ring-blue-300 dark:focus:ring-blue-800"
-            outline
-          >
+          <Button className="bg-gradient-to-br from-purple-600 to-blue-500 text-white hover:bg-gradient-to-bl focus:ring-blue-300 dark:focus:ring-blue-800">
             Sign In
           </Button>
         </Link>
