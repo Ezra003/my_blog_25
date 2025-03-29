b070897cc331a8763b6b4e187a28a86c12cc7c93
Ezra003 <126328199+Ezra003@users.noreply.github.com>
2025-03-29 21:28:34 +0100

    Add-next-themes-to-apply dark-mode : it dosent work for the Navbar

--- src/app/components/ThemeCom.jsx
+++ src/app/components/ThemeCom.jsx
@@ -5,14 +5,14 @@ import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
 
 export default function ThemeCom({ children }) {
-  const { theme, setTheme } = useTheme();
+  const {theme} = useTheme();
   const [mounted, setMounted] = useState(false);
   useEffect(() => setMounted(true), []);
   if (!mounted) return null;
 
   return (
     <div className={theme}>
-      <div className='bg-white text-gray-700 dark:text-gray-200 dark:bg-gray-900 min-h-screen'>
+      <div >
         {children}
       </div>
     </div>
